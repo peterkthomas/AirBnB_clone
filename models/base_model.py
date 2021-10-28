500f2b5700478caecfd8b0bc9ee171b46f35cd27
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-10-28 17:32:52 -0400

    added to dict and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,13 @@ class BaseModel(object):
         self.id = str(uuid4())
         self.updated_at = datetime.today()
         self.created_at = datetime.today()
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """String representation of self"""
@@ -23,3 +30,11 @@ class BaseModel(object):
     def save(self):
         """saves with the current date time"""
         self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """ returns a __dict__ of the instance"""
+        td = self.__dict__.copy()
+        td["created_at"] = self.created_at.isoformat()
+        td["updated_at"] = self.updated_at.isoformat()
+        td["__class__"] = self.__class__.__name__
+        return td
