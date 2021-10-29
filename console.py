100f82819c023408f8609f5c2773594124a47d5c
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-10-29 18:53:52 -0400

    initial console creation

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    File: console.py
+"""
+import cmd
+import sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNB Console Class"""
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """Exits the program"""
+        sys.exit()
+
+    def do_EOF(self, line):
+        """Catches EOF keyboard entry"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Makes it so an empty line doesn't do anything"""
+        print("", end="")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
