4ef7ea305fde21654ec52e0980317fcdc5734ab7
Weltopa <patrick.welton@yahoo.com>
2021-10-29 19:00:20 -0700

    create

--- console.py
+++ console.py
@@ -29,11 +29,23 @@ class HBNBCommand(cmd.Cmd):
         """Makes it so an empty line doesn't do anything"""
         print("", end="")
 
+    def do_create(self, arg):
+        """Creates new instance of BaseModel"""
+        new_arg = arg.split()
+        if len(new_arg) == 0:
+            print("** class name missing **")
+        elif new_arg[0] not in self.class_list:
+            print("** class doesn't exist **")
+        else:
+            new model = self.class_list[new_arg[0]]()
+            new_model.save()
+            print(new_model.id)
+
     def do_destroy(self, arg):
         new_arg = arg.split()
         class_id = new_arg[0] + '.' + new_arg[1]
         if len(new_arg) == 0:
-            print("** class name missing**")
+            print("** class name missing **")
         elif new_arg[0] not in self.class_list:
             print("** class doesn't exist **")
         elif len(new_arg) < 2:
